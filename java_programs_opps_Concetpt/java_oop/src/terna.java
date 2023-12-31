dc4eabad3de7b11ed5f2193e20fa79354eb6bcbc
MohitSojitra123 <mohitppatel2005@gmail.com>
2023-12-31 10:47:11 +0530

    xyz
    xyz

--- java_programs_opps_Concetpt/java_oop/src/terna.java
+++
@@ -1,8 +0,0 @@
-public class terna {
-    public static void main(String[] args) {
-        int a=1000,b=200,c=30;
-
-        int max=(a>b) ?(a>c ? a:c):(b>c ? b:c);
-        System.out.println(max);
-    }
-}
